b3f2b1cf1c13c4edcac62729d5c192682b12e914
matt-fang <matthewfang.calif@gmail.com>
2025-02-24 00:15:29 -0800

    Added onboarding
    Issues:
    - need to break up body for type-checking
    - need to check if eatingStyle is acftually saved

---
+++ happy-eat-infinity/Models/EatingStyle.swift
@@ -0,0 +1,8 @@
+//
+//  EatingStyle.swift
+//  happy-eat-5
+//
+//  Created by Matthew Fang on 2/23/25.
+//
+
+import Foundation
---
+++ happy-eat-infinity/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  happy-eat-5
+//
+//  Created by Matthew Fang on 2/23/25.
+//
+
+import Foundation
